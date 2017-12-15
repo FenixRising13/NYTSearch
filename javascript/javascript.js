2b24d27f4e080489215c40b31d67716a21dfc90d
Tamara Hall <tamarahall@Tamaras-MacBook-Pro.local>
2017-12-15 13:42:05 -0500

    quick save

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,28 @@
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-    // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
-    // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
 
-    console.log(response);
-  }
+
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "0a93f86f37d1478083a5b36b40c31054",
+  'begin_date': bgDate,
+  'end_date': "20171231",
+  'sort': "newest"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
+
+
+//We will need functions to take in what was typed in the submit boxes to call upon the response
+
+var bgDate =
+
+
+$("button").on("click", function() { 
+
+}
